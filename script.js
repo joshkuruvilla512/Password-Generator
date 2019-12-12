f86848a1db5de8ed15d6e8e81e4880e81d9cc6f1
Joshua Kuruvilla <joshkuruvilla512@gmail.com>
2019-12-11 20:08:19 -0600

    Initialized the password generator with the variables for upper and lowercase

---
+++ script.js
@@ -0,0 +1,11 @@
+function generate(length = 8){
+    var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lowercase = "abcdefghijklmnopqrsttuvwxyz";
+    var numbers = "0123456789";
+    var symbols = "!@#$%^&*()-_=+`~[{]}|;:'<,>.?/";
+
+    var all = uppercase + lowercase + numbers + symbols;
+
+    var password = " ";
+
+}
